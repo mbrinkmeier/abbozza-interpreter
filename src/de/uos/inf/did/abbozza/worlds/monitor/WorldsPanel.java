604c25e602ccf647058c594981d789ff6b88a5cb
mbrinkmeier <mbrinkmeier@uni-osnabrueck.de>
2018-11-29 16:29:54 +0100

    Merge origin/nightly into nightly
    Conflicts:
    	src/de/uos/inf/did/abbozza/handler/LoadHandler.java

--- src/de/uos/inf/did/abbozza/worlds/monitor/WorldsPanel.java
+++ src/de/uos/inf/did/abbozza/worlds/monitor/WorldsPanel.java
@@ -1,4 +1,3 @@
-<<<<<<< OURS
 /**
  * @license
  * abbozza!
@@ -116,59 +115,4 @@ public class WorldsPanel extends MonitorPanel {
     public void disconnect(AbbozzaMonitor monitor) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-=======
-/*
- * Copyright 2018 mbrin.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package de.uos.inf.did.abbozza.worlds.monitor;
-
-import de.uos.inf.did.abbozza.monitor.AbbozzaMonitor;
-import de.uos.inf.did.abbozza.monitor.MonitorPanel;
-import de.uos.inf.did.abbozza.monitor.clacks.ClacksBytes;
-import javax.swing.JPopupMenu;
-
-/**
- *
- * @author mbrin
- */
-public class WorldsPanel extends MonitorPanel {
-
-    @Override
-    public JPopupMenu getPopUp() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void processMessage(String msg) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void process(ClacksBytes bytes) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void connect(AbbozzaMonitor monitor) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void disconnect(AbbozzaMonitor monitor) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
->>>>>>> THEIRS
 }
